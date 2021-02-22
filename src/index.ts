3c36a9087d07745326c82f6555c2feb8f0e9c830
coderB <flack319@gmail.com>
2021-02-22 11:57:42 +0900

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import path from "path";
+import cors from "cors";
 
 const app: express.Express = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/', (req, res, next) => {
